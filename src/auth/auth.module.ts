33e0ea02f14092c39b05082b00db547a979d93c2
HellCounT <a_killaheadz@live.ru>
2023-05-28 18:55:08 +0300

    Users Repo rewritten, Entities imported to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,9 +10,23 @@ import { ConfigService } from '@nestjs/config';
 import { BasicStrategy } from './strategies/basic.strategy';
 import { EmailService } from '../email/email.service';
 import { UsersQuery } from '../users/users.query';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserGlobalBan } from '../users/etities/user-global-ban.entity';
+import { User } from '../users/etities/user.entity';
+import { UserConfirmation } from '../users/etities/user-confirmation.entity';
+import { UserRecovery } from '../users/etities/user-recovery.entity';
 
 @Module({
-  imports: [PassportModule, JwtModule.register({})],
+  imports: [
+    PassportModule,
+    JwtModule.register({}),
+    TypeOrmModule.forFeature([
+      User,
+      UserGlobalBan,
+      UserConfirmation,
+      UserRecovery,
+    ]),
+  ],
   providers: [
     LocalStrategy,
     JwtStrategy,
