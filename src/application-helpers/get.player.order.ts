2c3904f4b2bab7a461c9e29f90adb57a83fbfc50
HellCounT <a_killaheadz@live.ru>
2023-06-20 19:50:13 +0300

    h24: debugging wip

--- src/application-helpers/get.player.order.ts
+++ src/application-helpers/get.player.order.ts
@@ -1,6 +1,7 @@
 import { Game } from '../quiz/entities/game.entity';
 import { PlayerOrder } from './statuses';
 
+// перенести по архитектуре в игры
 export const getPlayerOrder = (game: Game, playerId: string): PlayerOrder => {
   let playerOrder;
   if (playerId === game.firstPlayerUserId) playerOrder = PlayerOrder.first;
