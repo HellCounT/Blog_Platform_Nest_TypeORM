df1793def39530f2f0dee6a0d4482a2afd04949d
HellCounT <a_killaheadz@live.ru>
2023-09-26 13:45:22 +0300

    h27: raw sql queries inserted. Need to check which one will work

--- src/quiz/games.query.ts
+++ src/quiz/games.query.ts
@@ -331,3 +331,63 @@ export class GamesQuery {
     return undefined;
   }
 }
+
+//todo: select the appropriate sql query and implement in ranking code
+
+// `
+// SELECT
+//   p.user_id AS id,
+//   u.login,
+//   SUM(fg.first_player_score) + SUM(sg.second_player_score) AS sum_score,
+//   AVG(fg.first_player_score + sg.second_player_score) AS avg_score,
+//   COUNT(fg.id) + COUNT(sg.id) AS games_count,
+//   SUM(CASE WHEN fg.player_finished_first = 'FIRST_PLAYER' THEN 1 ELSE 0 END) +
+//   SUM(CASE WHEN sg.player_finished_first = 'SECOND_PLAYER' THEN 1 ELSE 0 END) AS wins_count,
+//   SUM(CASE WHEN fg.player_finished_first = 'SECOND_PLAYER' THEN 1 ELSE 0 END) +
+//   SUM(CASE WHEN sg.player_finished_first = 'FIRST_PLAYER' THEN 1 ELSE 0 END) AS losses_count,
+//   SUM(CASE WHEN fg.player_finished_first IS NULL THEN 1 ELSE 0 END) +
+//   SUM(CASE WHEN sg.player_finished_first IS NULL THEN 1 ELSE 0 END) AS draws_count
+// FROM player p
+// LEFT JOIN "user" u ON u.id = p.user_id
+// LEFT JOIN game fg ON fg.first_player_user_id = p.user_id
+// LEFT JOIN game sg ON sg.second_player_user_id = p.user_id
+// GROUP BY p.user_id, u.login
+// ORDER BY avg_score DESC, sum_score DESC
+// LIMIT 10;
+// `;
+//
+// `
+// WITH player_scores AS (
+//   SELECT
+//     p."userId" AS player_id,
+//     p."userId" AS user_id,
+//     SUM(CASE WHEN g."firstPlayerUserId" = p."userId" THEN g."firstPlayerScore" ELSE g."secondPlayerScore" END) as total_score,
+//     COUNT(g.id) as games_count,
+//     AVG(CASE WHEN g."firstPlayerUserId" = p."userId" THEN g."firstPlayerScore" ELSE g."secondPlayerScore" END) as average_score,
+//     COUNT(CASE WHEN ((g."firstPlayerUserId" = p."userId" AND g."playerFinishedFirst" = 'first') OR
+//                      (g."secondPlayerUserId" = p."userId" AND g."playerFinishedFirst" = 'second')) THEN 1 END) as wins_count,
+//     COUNT(CASE WHEN ((g."firstPlayerUserId" = p."userId" AND g."playerFinishedFirst" = 'second') OR
+//                      (g."secondPlayerUserId" = p."userId" AND g."playerFinishedFirst" = 'first')) THEN 1 END) as losses_count,
+//     COUNT(CASE WHEN g."playerFinishedFirst" = 'draw' THEN 1 END) as draws_count
+//   FROM
+//     "Player" p
+//     LEFT JOIN "Game" g ON g."firstPlayerUserId" = p."userId" OR g."secondPlayerUserId" = p."userId"
+//   GROUP BY
+//     p."userId"
+// ),
+// player_ranks AS (
+//   SELECT
+//     ps.*,
+//     u.login,
+//     ROW_NUMBER() OVER(ORDER BY ps.average_score DESC, ps.total_score DESC) as rank
+//   FROM
+//     player_scores ps
+//     INNER JOIN "User" u ON u.id = ps.user_id
+// )
+// SELECT
+//   *
+// FROM
+//   player_ranks
+// WHERE
+//   rank > $1 AND rank <= $2;
+// `
