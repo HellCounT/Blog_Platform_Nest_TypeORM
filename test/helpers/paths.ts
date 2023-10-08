0bbbd44d77860f6b4f4a10d0746105bc73df2ceb
HellCounT <a_killaheadz@live.ru>
2023-10-08 11:30:56 +0300

    h28: 1st test

--- test/helpers/paths.ts
+++ test/helpers/paths.ts
@@ -5,21 +5,10 @@ export const quizGamePath = '/pair-game-quiz/pairs';
 
 export const authPath = '/auth';
 export const authLoginPath = authPath + '/login';
-export const authMePath = authPath + '/me';
-export const authRegistrationPath = authPath + '/registration';
-export const authRefreshTokenPath = authPath + '/refresh-token';
-export const authLogoutPath = authPath + '/logout';
-export const authPasswordRecoveryPath = authPath + '/password-recovery';
-export const authNewPasswordPath = authPath + '/new-password';
-export const authRegistrationConfirmationPath =
-  authPath + '/registration-confirmation';
-export const authRegistrationEmailResendingPath =
-  authPath + '/registration-email-resending';
 
 export const bloggerBlogsPath = '/blogger/blogs';
 export const bloggerUsersPath = '/blogger/users';
 
-export const postsPath = '/posts';
 export const blogsPath = '/blogs';
 export const commentsPath = '/comments';
 export const securityPath = '/security/devices';
